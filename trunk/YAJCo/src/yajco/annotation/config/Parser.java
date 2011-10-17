577cdbd2e1cbf5e7d5c591467baac7ec3d3ca869
deel77 <deel77@29de7404-172d-11df-9ac1-3b3d05a0ff07>
2011-10-17 08:38:22 +0000

    podpora generovania JavaCC Parsera z aktualneho modelu

--- trunk/YAJCo/src/yajco/annotation/config/Parser.java
+++ trunk/YAJCo/src/yajco/annotation/config/Parser.java
@@ -10,7 +10,7 @@ import java.lang.annotation.Target;
 public @interface Parser {
 
 	@Deprecated
-        String className();
+        String className() default "";
 
 	String mainNode() default "";
 
