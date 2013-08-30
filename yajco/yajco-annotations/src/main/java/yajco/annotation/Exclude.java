a9b8f18720adb8327902ccdefb714df939667c06
deel77@gmail.com <deel77@gmail.com@29de7404-172d-11df-9ac1-3b3d05a0ff07>
2013-08-30 09:18:36 +0000

    pridanie moznosti exclude

--- yajco/yajco-annotations/src/main/java/yajco/annotation/Exclude.java
+++ yajco/yajco-annotations/src/main/java/yajco/annotation/Exclude.java
@@ -8,4 +8,9 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE, ElementType.METHOD, ElementType.CONSTRUCTOR})
 public @interface Exclude {
+    /**
+     * Use to exclude some specific concepts from a language.
+     * @return 
+     */
+    String[] concept() default {};
 }
